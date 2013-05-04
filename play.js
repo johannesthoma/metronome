a9c1a65c00a78b99bf6ce6fbe93502464a8bc2dd
Johannes Thoma <johannes.thoma@rise-world.com>
2013-05-04 14:07:14 +0200

    Does not work.

--- play.js
+++ play.js
@@ -1,24 +1,29 @@
 // var timer = window.setInterval(function() { beep(); }, 1000);
 
 var started = false;
-var debug = document.getElementById("debug");
+var visual = document.getElementById("visual");
+var beeper = document.getElementById("beep");
 
 function beep() {
-debug.innerHTML = "A";
-	document.getElementById("beep").play();
-debug.innerHTML = "B";
+	visual.innerHTML = (visual.innerHTML == "X") ? "O" : "X";
+/*	alert(beeper.seekable);
+	beeper.pause();
+	beeper.currentTime = 0.0;
+	beeper.play(); */
 }
 
 function tick() {
+//	alert("tick");
 	var next_tick_bpm = parseInt(document.getElementById("bpm").value);
-	if (started) {
-		if (next_tick_bpm > 0) {
-		 	var next_tick_ms = 60000 / next_tick_bpm;
-			beep();
-			setTimeout(tick, next_tick_ms);
-		} else {
+	if (started && next_tick_bpm > 0) {
+//		if (next_tick_bpm > 0) {
+	 	var next_tick_ms = 60000 / next_tick_bpm;
+		beep();
+//	alert("setting timeout "+next_tick_ms);
+		setTimeout(tick, next_tick_ms);
+/*		} else {
 			toggle();
-		}
+		} */
 	}
 }
 

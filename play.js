86ab461b904ffd6e85966b2969d0fbfba2148245
Johannes Thoma <johannes.thoma@rise-world.com>
2013-05-04 13:35:37 +0200

    Handling bpm = 0, stop if it is.

--- play.js
+++ play.js
@@ -1,17 +1,24 @@
 // var timer = window.setInterval(function() { beep(); }, 1000);
 
 var started = false;
+var debug = document.getElementById("debug");
 
 function beep() {
+debug.innerHTML = "A";
 	document.getElementById("beep").play();
+debug.innerHTML = "B";
 }
 
 function tick() {
 	var next_tick_bpm = parseInt(document.getElementById("bpm").value);
-	if (started && next_tick_bpm > 0) {
-	 	var next_tick_ms = 60000 / next_tick_bpm;
-		beep();
-		setTimeout(tick, next_tick_ms);
+	if (started) {
+		if (next_tick_bpm > 0) {
+		 	var next_tick_ms = 60000 / next_tick_bpm;
+			beep();
+			setTimeout(tick, next_tick_ms);
+		} else {
+			toggle();
+		}
 	}
 }
 
